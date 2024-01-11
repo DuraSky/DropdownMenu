3c3f5d7a6177c85ac634cfe03cd19565d48aea41
Lukas Broskovic <lukas.broskovic@gmail.com>
2024-01-11 10:43:08 +0100

    created very simple dropdown menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import "./style.css"
 
-console.log("Hi")
+const menuImg = document.getElementById("menuImg")
+const menuContent = document.querySelector(".expandable")
+
+menuImg.addEventListener("click", ()=>{
+    if(menuContent.classList.contains("hidden")){
+        menuContent.classList.remove("hidden");
+        menuContent.classList.add("visible")
+    }else{
+        menuContent.classList.remove("visible")
+
+        menuContent.classList.add("hidden");
+    }
+})
